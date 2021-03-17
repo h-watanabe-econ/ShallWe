805d0ea2ebcf4777a6bb590e285223d4e57b1dda
h-watanabe-econ <76144195+h-watanabe-econ@users.noreply.github.com>
2021-03-17 12:35:40 +0900

    Add files via upload

---
+++ ShallWe/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Event,Friend,Group,Good
+admin.site.register(Event)
+admin.site.register(Friend)
+admin.site.register(Group)
+admin.site.register(Good)
+# Register your models here.
---
+++ ShallWe/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ShallweConfig(AppConfig):
+    name = 'ShallWe'
